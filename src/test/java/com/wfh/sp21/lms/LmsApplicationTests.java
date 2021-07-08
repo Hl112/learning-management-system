9efc03d7ebd31d82e86972c636e7d977162421fc
hl112 <forest.tl112@gmail.com>
2021-07-08 12:01:40 +0700

    First Init

---
+++ src/test/java/com/wfh/sp21/lms/LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wfh.sp21.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
